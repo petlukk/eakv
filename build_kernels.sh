b9569a4747ef295030c5274b258c8eea34e7e59a
Peter Lukka <peter.lukka@gmail.com>
2026-03-10 07:35:22 +0000

    build: add .o object file output for libeakv static linking

--- build_kernels.sh
+++ build_kernels.sh
@@ -101,4 +101,31 @@ echo "    -> fused_attention (AVX-512, experimental fused softmax+attention)"
 "$EA" "$KERNEL_DIR/fused_k_score_gqa.ea" --lib --avx512 -o "$LIB_DIR/${PREFIX}fused_k_score_gqa${EXT}"
 echo "    -> fused_k_score_gqa (AVX-512, GQA loop-flipped K-score + V-sum)"
 
-echo "Done. Libraries in $LIB_DIR, bindings in $BIND_DIR"
+# Object files for static linking into libeakv (emit asm, then assemble)
+OBJ_DIR="$SCRIPT_DIR/build/obj"
+mkdir -p "$OBJ_DIR"
+
+echo "  Compiling kernel object files for libeakv..."
+
+emit_obj() {
+    local src="$1" name="$2" flags="${3:-}"
+    # shellcheck disable=SC2086
+    "$EA" "$src" --emit-asm $flags
+    local asm_file
+    asm_file="$(basename "${src%.ea}.s")"
+    gcc -c "$asm_file" -o "$OBJ_DIR/$name.o"
+    rm "$asm_file"
+    echo "    -> $name.o"
+}
+
+emit_obj "$KERNEL_DIR/quantize_simd.ea" quantize_simd
+emit_obj "$KERNEL_DIR/validate.ea" validate
+emit_obj "$KERNEL_DIR/dequantize_simd.ea" dequantize_sse
+emit_obj "$KERNEL_DIR/dequantize_avx2.ea" dequantize_avx2
+emit_obj "$KERNEL_DIR/dequantize_avx512.ea" dequantize_avx512 --avx512
+emit_obj "$KERNEL_DIR/fused_k_score.ea" fused_k_score --avx512
+emit_obj "$KERNEL_DIR/fused_v_sum.ea" fused_v_sum --avx512
+emit_obj "$KERNEL_DIR/fused_attention.ea" fused_attention --avx512
+emit_obj "$KERNEL_DIR/fused_k_score_gqa.ea" fused_k_score_gqa --avx512
+
+echo "Done. Libraries in $LIB_DIR, objects in $OBJ_DIR, bindings in $BIND_DIR"
